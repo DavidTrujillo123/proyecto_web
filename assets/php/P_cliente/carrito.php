c41b72af76de08df9fb10865b21f00972e498bb6
Dario21042 <dnns2723@gmail.com>
2023-02-15 19:03:30 -0500

    css
    Tablas

--- assets/php/P_cliente/carrito.php
+++ assets/php/P_cliente/carrito.php
@@ -1,4 +1,10 @@
 <?php
+include("../Conexion.php");
+$db = new Conexion();
+$sql = "SELECT * FROM `cliente`";
+$res = $db->OperSql($sql);
+$num_clientes = $res->num_rows;
+$Total=0;
 session_start();
 if (!isset($_SESSION['usuario'])) { //Si no existe una sesión activa
     echo '
@@ -11,89 +17,54 @@ if (!isset($_SESSION['usuario'])) { //Si no existe una sesión activa
     die();
 }
 
-include("../Conexion.php");
-$db = new Conexion();
-
 $usuario = $_SESSION['usuario']; //asigna la sesión al usuario
+$db = new Conexion();
 //busca la cedula, sea con el nombre de usuario o email en la tabla usuario
-$sql1 = "SELECT id_usuario FROM usuario WHERE usuario LIKE '$usuario' or email LIKE '$usuario'";
+$sql1 = "SELECT id_usuario FROM usuario WHERE usuario= '$usuario' or email='$usuario'";
 $cliente = $db->Read($sql1);
-$id_usuario = $cliente[0]['id_usuario'];//id usuario dependiendo de la sesion
+$id_usuario = $cliente[0]['id_usuario'];
 
 $sql_carrito = "SELECT id_carrito FROM carrito WHERE carrito.id_usuario=$id_usuario";
 $cart = $db->Read($sql_carrito);
-$id_carrito = $cart[0]['id_carrito'];//carrito del usuario
+$id_carrito = $cart[0]['id_carrito'];
 
 
 if (isset($_GET['agregar'])) {
-    $agregar = $_GET['agregar'];//codigo_producto
-    
-    $sql2 = "SELECT * FROM `carrito_item` 
-            WHERE carrito_item.id_carrito=$id_carrito 
-            AND carrito_item.codigo_producto='$agregar'";
-
+    $sql2 = "SELECT * FROM `carrito_item` WHERE carrito_item.id_carrito=$id_carrito AND carrito_item.codigo_producto=$_GET[agregar]";
     $res = $db->OperSql($sql2);
     $num_productos = $res->num_rows;
-    
-    if ($num_productos == 0) { 
+    if ($num_productos == 0) {
         $sql = "INSERT INTO 
-                carrito_item(id_carrito, codigo_producto, 
-                cantidad_cliente, subtotal)
-                VALUES ('$id_carrito','$agregar','1',
-                (SELECT precio FROM producto 
-                WHERE codigo_producto LIKE '$agregar'))";
+    carrito_item(id_carrito_item, id_carrito, codigo_producto, cantidad_cliente, subtotal)
+     VALUES (null,'$id_carrito',$_GET[agregar],'1',(SELECT precio FROM producto WHERE codigo_producto=$_GET[agregar])*1);";
         $prod_carrito = $db->OperSql($sql);
     } else {
         $fila = mysqli_fetch_array($res);
         $cantidad = $fila['cantidad_cliente'] + 1;
-
-        $sql = "UPDATE carrito_item 
-                SET carrito_item.cantidad_cliente=$cantidad , 
-                carrito_item.subtotal=(SELECT precio
-                    FROM producto 
-                    WHERE codigo_producto='$agregar')*$cantidad 
-                    WHERE carrito_item.codigo_producto LIKE '$agregar' 
-                    AND carrito_item.id_carrito=$id_carrito";
+        $sql = "UPDATE carrito_item SET carrito_item.cantidad_cliente=$cantidad , carrito_item.subtotal=(SELECT precio FROM producto WHERE codigo_producto=$_GET[agregar])*$cantidad WHERE carrito_item.codigo_producto=$_GET[agregar] AND carrito_item.id_carrito=$id_carrito";
         $prod_carrito = $db->OperSql($sql);
     }
+    echo ("<script> 
+    window.location.href = '../../../pagina_cliente.php#product';
 
-    echo (" <script> 
-                window.location.href = '../../../pagina_cliente.php#product';
-            </script> ");
+    </script> ");
 }
 if (isset($_POST['id1'])) {
 
-    $id1 = $_POST['id1'];
-
-    $sql1 = "SELECT cantidad 
-            FROM producto 
-            WHERE codigo_producto= '$id1' ";
-
+    $sql1 = "SELECT cantidad FROM producto WHERE codigo_producto= $_POST[id1] ";
     $cantidad = $db->Read($sql1);
     $cantidadUnit = $cantidad[0]['cantidad'];
-
-    //comprueba si la cantidad es igual o menor a la cantidad de stock
     if ($_POST['cantidad'] <= $cantidadUnit) {
-        $cantidad = $_POST['cantidad'];
-
-        $sql = "UPDATE carrito_item 
-                SET carrito_item.cantidad_cliente=$cantidad , 
-                carrito_item.subtotal=(SELECT precio 
-                    FROM producto 
-                    WHERE codigo_producto='$id1')*$cantidad 
-                    WHERE carrito_item.codigo_producto='$id1' 
-                    AND carrito_item.id_carrito=$id_carrito";
+        $sql = "UPDATE carrito_item SET carrito_item.cantidad_cliente=$_POST[cantidad] , carrito_item.subtotal=(SELECT precio FROM producto WHERE codigo_producto=$_POST[id1])*$_POST[cantidad] WHERE carrito_item.codigo_producto=$_POST[id1] AND carrito_item.id_carrito=$id_carrito";
         $prod_carrito = $db->OperSql($sql);
     } else {
         echo ("<script> 
-                    alert('Cantidad Insuficiente en stock !');
-                </script> ");
+    alert('Cantidad Insuficiente en stock !');
+    </script> ");
     }
 }
-//Elimina produtos del carrito
 if (isset($_GET['eliminar'])) {
-    $sql = "DELETE FROM `carrito_item` 
-            WHERE carrito_item.id_carrito_item=$_GET[eliminar]";
+    $sql = "DELETE FROM `carrito_item` WHERE carrito_item.id_carrito_item=$_GET[eliminar]";
     $eliminar_prod = $db->OperSql($sql);
 }
 
@@ -127,10 +98,12 @@ if (isset($_GET['eliminar'])) {
 
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
-
+    <link rel="stylesheet" href="../../css/tabla.css" />
 </head>
 
-<body>
+<body class="principal"  >
+    <div style="overflow-x: auto";>
+    <div id="tabla">
     <?php
     include('../../../footer_nav_cliente/nav_clientes.php');
 
@@ -142,57 +115,70 @@ if (isset($_GET['eliminar'])) {
             <th>Precio Unitario</th>
             <th>Cantidad</th>
             <th>subtotal</th>
-            <th>Eliminar</th>
+            <th>Acciones</th>
         </tr>
         <?php
         $sql = "SELECT * FROM `producto`";
         $productos = $db->OperSql($sql);
 
-        $sql2 = "SELECT * FROM producto,carrito_item,carrito 
-                WHERE carrito_item.id_carrito=carrito.id_carrito 
-                AND producto.codigo_producto=carrito_item.codigo_producto 
-                AND carrito.id_usuario=$id_usuario ";
-        $Total = 0;
+        $sql2 = "SELECT * FROM producto,carrito_item,carrito WHERE carrito_item.id_carrito=carrito.id_carrito 
+        AND producto.codigo_producto=carrito_item.codigo_producto AND carrito.id_usuario=$id_usuario ";
+      
         $carrito = $db->OperSql($sql2);
-
         foreach ($carrito as $row) {
             $Total = $Total + $row['subtotal'];
+
+
+
             echo '
               <form action="carrito.php" method="post" >
+
                 <tr>
                     <td><img heigth="30px" src="data:image/jpg;base64,' . base64_encode($row['img']) . '"/></td>
                     <td>' . $row['nombre_producto'] . '</td>
                     <td>' . $row['precio'] . '</td>
                     <td> <input value="' . $row['cantidad_cliente'] . '" type="number" name="cantidad">
-                    <input type="hidden"  value="' . $row['codigo_producto'] . '" name="id1">
+                    <input id="ojo" type="hidden"  value="' . $row['codigo_producto'] . '" name="id1">
                     
                     </td>
                     <td>' . $row['subtotal'] . '</td>
-                    <td><input type="submit" value="Actualizar Cantidad" ></td>
-                    <td><a href="#" onclick="eliminar(' . $row['id_carrito_item'] . ')">Eliminar</td>
-
+                    <td><input type="submit" value="Actualizar" ><a href="#" onclick="eliminar(' . $row['id_carrito_item'] . ')">Eliminar</td>
+                    
 
 
                 </tr>
                 </form>
 
-                
-                
+                      
                 
                 ';
         }
-
-
-
         ?>
+        <tr>
+            <td><h2>Total:</h2></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><?php echo $Total ?></td>
+            <td><a href="pago.php"><button id="compra">Comprar ahora</button></a></td>
+        </tr>
+      
     </table>
-    <?php echo $Total; ?>
-    <?php
+
+            
+        
+    </div>
+   
+    </div>
+
+   <?php
     include("../../../footer_nav_cliente/footer.php");
     ?>
 
 
 </body>
+
+
 <script src="../../js/paginaCliente.js" defer></script>
 
 <script>
