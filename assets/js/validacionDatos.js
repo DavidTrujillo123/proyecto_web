27a24ad9e0da723397260be474073c097a2901ea
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2023-02-16 21:13:26 -0500

    Update validacionDatos.js

--- assets/js/validacionDatos.js
+++ assets/js/validacionDatos.js
@@ -22,7 +22,7 @@ var expresiones = {
 	nombre: /^[A-Z][a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
 	contraseña: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,12}$/, // 8 caracteres con mayusculas y minusculas
 	correo: /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/,
-    direccion: /^[A-ZÁÉÍÓÚÑ][A-Za-zÁÉÍÓÚáéíóúñÑ0-9,.\s]{3,16}$/  //letras, números, punto y guion
+    direccion: /^[A-ZÁÉÍÓÚÑ][A-Za-zÁÉÍÓÚáéíóúñÑ0-9,.\s]{3,16}$/  //letras, números, punto, coma  
 
 }
 
